2366bbe11919a117a9c98da5d67400649e344121
rahul <rahulkarki0608@gmail.com>
2026-03-30 18:02:57 +0530

    ok

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { MantineProvider } from "@mantine/core";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./context/AuthContext";
 
+console.log("Google Client ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <React.StrictMode>
