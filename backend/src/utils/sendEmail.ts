c65469484e2f5a443c1f1c95532b75a52cbcfee6
rahul <rahulkarki0608@gmail.com>
2026-03-30 13:55:59 +0530

    ye s

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -7,8 +7,6 @@ export const sendEmail = async (to: string, link: string) => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    connectionTimeout: 5000,
-    socketTimeout: 5000,
   });
 
   await transporter.sendMail({
